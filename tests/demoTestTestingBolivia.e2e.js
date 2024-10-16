044480d0e851109db136fc58a4d185a6468c5f56
mmanicera <mmanicera@genexusconsulting.com>
2024-10-15 21:36:08 -0300

    first commit

---
+++ tests/demoTestTestingBolivia.e2e.js
@@ -0,0 +1,13 @@
+const { $,expect,driver } = require('@wdio/globals');
+
+
+
+
+describe('WebdriverIO Mobile App Demo', ( ) => {
+    it('SauceLabs App Test', async () => {
+
+      await  driver.pause(10000)
+    
+
+    });
+})
